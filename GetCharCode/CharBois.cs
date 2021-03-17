e8ac39aaf1ee7a59fe8eb9ef43b083326ba66102
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-03-17 16:42:56 +0100

    Initial commit

---
+++ GetCharCode/CharBois.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GetCharCode
+{
+    public class CharBois
+    {
+        private char _char;
+        private string _string;
+
+        public CharBois(char char1)
+        {
+            _char = char1;
+        }
+
+        public void InvertCase()
+        {
+            if (char.IsUpper(_char)) _string = _char.ToString().ToLower();
+            else _string = _char.ToString().ToUpper();
+        }
+
+        public void ReConvert()
+        {
+            _char = Convert.ToChar(_string);
+        }
+
+        public int GetASCII() => _char;
+    }
+}
---
+++ GetCharCode/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GetCharCode
+{
+    class Program
+    {
+        // https://edabit.com/challenge/zaokQWNdEudmFWpk7
+        // SOLVED
+
+        static void Main(string[] args)
+        {
+            var c = new CharBois('a');
+            c.InvertCase();
+            c.ReConvert();
+            Console.Write(c.GetASCII());
+        }
+    }
+}
